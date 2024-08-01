4548cc65eced1b92437a56dd001a52259eab7b05
Pratik <pratik127001@gmail.com>
2024-08-01 17:28:48 +0530

    added Pagination

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+
+  const handlePrev = () => {
+    if (pageNo === 1) {
+      setPageNo(pageNo);
+    } else {
+      setPageNo(pageNo - 1);
+    }
+  };
+
+  const handleNext = () => {
+    setPageNo(pageNo + 1);
+  };
 
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=845e62b0e59f9c0af46e4904c2819d89&language=en-US&page=1`
+        `https://api.themoviedb.org/3/movie/popular?api_key=845e62b0e59f9c0af46e4904c2819d89&language=en-US&page=${pageNo}`
       )
       .then(function (res) {
         setMovies(res.data.results);
       });
-  }, []);
+  }, [pageNo]);
 
   return (
     <div className="p-5">
-      <div className="text-2xl m-5 font-bold text-center ">
-        Trending Movies
-      </div>
+      <div className="text-2xl m-5 font-bold text-center ">Trending Movies</div>
 
       <div className="flex flex-row flex-wrap  justify-around gap-8">
         {movies.map((movieObj) => {
-          return <MovieCard poster_path={movieObj.poster_path} name={movieObj.original_title}/>;
+          return (
+            <MovieCard
+              poster_path={movieObj.poster_path}
+              name={movieObj.original_title}
+            />
+          );
         })}
       </div>
+
+      <Pagination
+        pageNo={pageNo}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+      />
     </div>
   );
 }
