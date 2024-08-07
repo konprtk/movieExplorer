4e8935debac0dd82a2050e7d4e49e0a58502827b
Pratik <pratik127001@gmail.com>
2024-08-07 11:11:34 +0530

    Designed Watchlist component, Added delete from watchlist, populated watchlist

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,7 @@ function App() {
                   handleAddToWatchList={handleAddToWatchList}
                   handleRemoveFromWatchList={handleRemoveFromWatchList}
                 />
+                
               </>
             }
           />
